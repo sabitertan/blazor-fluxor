5fb6725d49640a37457e0eb0f2b4b328c9983962
Peter Morris <mrpmorris@gmail.com>
2019-09-20 20:30:36 +0100

    #82 Store initialization does not work in safari (#87)
    * Change InitializeFluxor script

---
+++ src/Blazor.Fluxor/content/index.js
@@ -0,0 +1,10 @@
+﻿const TryInitializeFluxor = function () {
+	var initializeFluxorScriptElement = document.getElementById("InitializeFluxor");
+	if (initializeFluxorScriptElement) {
+		const script = initializeFluxorScriptElement.innerHTML.replace("<!--!-->", "");
+		eval(script);
+	} else {
+		setTimeout(() => TryInitializeFluxor(), 100);
+	}
+};
+TryInitializeFluxor();	
